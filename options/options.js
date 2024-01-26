95f3d36345d79123c729a76105d78bfe3cfc398d
Crowdbotics <team@crowdbotics.com>
2024-01-26 20:46:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aps-learnlink-45612",
+  url: "https://aps-learnlink-45612.botics.co",
+  api: "https://aps-learnlink-45612.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
